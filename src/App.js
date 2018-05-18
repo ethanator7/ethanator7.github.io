7cfeb1af0af1b86f39473c8525b0912838bb0740
ethanator7 <ethanator7@icloud.com>
2018-05-18 17:10:07 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'tachyons';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      	<h1>Checkers</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
